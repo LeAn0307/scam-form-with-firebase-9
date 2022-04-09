85eb2d425dcad14ba8029ff9f5aa649b3cf04c17
An <lethixuanan2012@gmail.com>
2022-04-10 01:53:05 +0700

    firebase setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6NVGvqE9w5yUd745aGpieGk34sAcmm0k",
+  authDomain: "scam-form-firebase-9.firebaseapp.com",
+  projectId: "scam-form-firebase-9",
+  storageBucket: "scam-form-firebase-9.appspot.com",
+  messagingSenderId: "269036197931",
+  appId: "1:269036197931:web:78f8a85ada87719ba5726e",
+};
+firebase.initializeApp(firebaseConfig);
